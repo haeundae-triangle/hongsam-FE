eaa8f1264c3b76fc163ce6af9c5411d3b7c8719f
chadireoroonu <chadireoroonu@gmail.com>
2024-05-22 18:08:52 +0900

    Feat : 선택 태그 디자인 구현

--- src/components/Main/GameFilter.jsx
+++ src/components/Main/GameFilter.jsx
@@ -4,15 +4,15 @@ import { Games } from './Games'
 
 // 게임 소개
 export const GameFilter = () => {
-  const [listTag, setListTag] = useState('어서와, 술자리는 처음이지?')
-  const listTags = ['어서와, 술자리는 처음이지?', '너 게임 좀 하냐?', '고수만 덤벼', '노래부르는 게임',
-   '아이엠 그라운드 게임', '운빨게임', '두뇌 게임', '순발력 게임', '한명 죽이기 게임', '웃음 참기 게임']
+  const [listTag, setListTag] = useState('노래 부르는 게임')
+  const listTags = ['노래 부르는 게임', '아이엠 그라운드 게임', '운빨게임',
+   '두뇌 게임', '순발력 게임', '한명 죽이기 게임', '웃음 참기 게임']
 
   return (
     <Container>
       <TagContianer>
         {listTags.map((tag, index) => (
-          <Tag key={index} onClick={() => setListTag(tag)}>{tag}</Tag>
+          <Tag key={index} isSelected={listTag === tag} onClick={() => setListTag(tag)}>{tag}</Tag>
         ))}
       </TagContianer>
       <Games listFilter={listTag}></Games>
@@ -38,8 +38,8 @@ gap: 10px;
 `
 
 const Tag = styled.div`
-  color: #f3f3f3;
-  padding: 5px 10px;
-  border-radius: 5px;
-  border: 1px solid #f3f3f3;
+  color: ${({ isSelected }) => (isSelected ? '#f8f8f8' : '#aeaeae')};
+  padding: 8px 15px;
+  border-radius: 10px;
+  background-color: ${({ isSelected }) => (isSelected ? '#645FFF' : '#686868')};
 `;
