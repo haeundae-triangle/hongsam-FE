a64959b99ae44c0bc7634a3c33354614ed621c21
chadireoroonu <chadireoroonu@gmail.com>
2024-05-22 17:51:15 +0900

    Feat : 게임 리스트 만들러 가기 베너 업데이트

--- src/components/Banners/GameListBanner.jsx
+++ src/components/Banners/GameListBanner.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom';
+import GameBannerImage from '../../assets/GameBannerImage.png';
+import { CaretRightFill } from 'react-bootstrap-icons';
 
 // 게임 리스트 만들러가기 고정 베너
 export const GameListBanner = () => {
@@ -13,8 +15,16 @@ export const GameListBanner = () => {
   return (
     <Container>
       <BannerContainer onClick={handleClick}>
-        <H4>홍삼으로 같이 할 게임 공유하고, 함께 술자리 즐겨요</H4>
-        <H3>홍삼과 함께라면, 어떤 술게임도 어렵지 않아요!</H3>
+        <ImageContainer>
+
+        </ImageContainer>
+        <TextContainer>
+          <H4>홍삼으로 같이 할 게임 공유하고, 함께 술자리 즐겨요</H4>
+          <H3>홍삼과 함께라면, 어떤 술게임도 어렵지 않아요!</H3>
+        </TextContainer>
+        <IconContainer>
+          <CaretRightFill size={24} color="#222222"/>
+        </IconContainer>
       </BannerContainer>
     </Container>
   );
@@ -34,20 +44,41 @@ const BannerContainer = styled.div`
   height: 100px;
   border-radius: 15px;
   display: flex;
+  flex-direction: row;
+`
+
+const ImageContainer = styled.div`
+  width: 20%;
+  background-image: url('${GameBannerImage}');
+  background-size: cover;
+  background-position: center;
+`
+
+const TextContainer = styled.div`
   flex-direction: column;
   justify-content: center;
+  width: 70%;
+  margin: 4.5% 0;
+  // background-color: red;
 `
 
 const H4 = styled.h4`
   color: #FF5171;
   font-size: 14px;
-  margin: 2% 0;
   text-align: center;
+  margin: 2% 0;
 `;
 
 const H3 = styled.h3`
   color: #222222;
   font-size: 16px;
-  margin: 2% 0;
   text-align: center;
+  margin: 3% 0;
 `;
+
+const IconContainer = styled.div`
+  width: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
