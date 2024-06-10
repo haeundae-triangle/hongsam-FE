cb2d8d5a051d9d731e37c5ca944de1a471b13136
chadireoroonu <chadireoroonu@gmail.com>
2024-06-10 21:50:39 +0900

    Feat : 검색 페이지 기본 컴포넌트 설계

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
 
+import ToolbarTopSide from "../components/ToolbarTopside";
+import { SearchBar } from "../components/SearchBar";
+
 const Search = () => {
   const location = useLocation();
   const { searchTerm } = location.state;
@@ -9,7 +12,14 @@ const Search = () => {
 
   return (
     <Container>
-      <H2>{searchWord}</H2>
+      {/* MakeLink/TopBar 외부로 빼서 가져오기 */}
+      <SearchBar initialSearchTerm={searchTerm} />
+      <HistoryContainer>
+        <H3>최근 검색어</H3>
+      </HistoryContainer>
+      <RecommendCointainer>
+        <H3>추천 게임</H3>
+      </RecommendCointainer>
     </Container>
   );
 };
@@ -18,9 +28,21 @@ export default Search;
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   height: auto;
   min-height: 100vh;
   background-color: #222222;
+`
+
+const HistoryContainer = styled.div`
+  height: auto;
+  min-height: 80px;
+  margin: 6%;
+`
+
+const RecommendCointainer = styled.div`
+  height: auto;
+  margin: 6%;
 `
 
 const H2 = styled.h2`
@@ -28,4 +50,10 @@ const H2 = styled.h2`
   font-size: 25px;
   text-align: center;
   justify-align: center;
+`
+
+const H3 = styled.h4`
+  color: #645FFF;
+  font-size: 16px;
+  margin: 0;
 `
