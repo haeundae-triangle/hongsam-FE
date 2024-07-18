d7702f5b0719944f1918dfd19ac01f223f6e40bd
chadireoroonu <chadireoroonu@gmail.com>
2024-07-18 22:27:36 +0900

    Refactor : 변수 사용 변경

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,7 +2,7 @@ export const ADD_GAME = 'ADD_GAME';
 export const REMOVE_GAME = 'REMOVE_GAME';
 
 export const addGame = (game) => ({
-    type: "ADD_GAME",
+    type: ADD_GAME,
     payload: game,
   });
 
