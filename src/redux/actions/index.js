89e6f3bc5bd5e23b0913e8f951a3213180ca372c
chadireoroonu <chadireoroonu@gmail.com>
2024-07-19 21:56:02 +0900

    Feat : redux에 select 기능 추가

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,10 +1,16 @@
 export const ADD_GAME = 'ADD_GAME';
+export const SELECT_GAME = 'SELECT_GAME';
 export const REMOVE_GAME = 'REMOVE_GAME';
 
 export const addGame = (game) => ({
-    type: ADD_GAME,
-    payload: game,
-  });
+  type: ADD_GAME,
+  payload: game,
+});
+
+export const selectGame = (gameId) => ({
+  type: SELECT_GAME,
+  payload: gameId,
+});
 
 export const removeGame = (gameId) => ({
   type: REMOVE_GAME,
