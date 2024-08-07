5dacb3679d69e3db429e14ff407fcd4f2d6c1388
chadireoroonu <chadireoroonu@gmail.com>
2024-08-07 18:39:27 +0900

    Style : 콘솔로그 주석 처리

--- src/gameAPI/fetchEachServiceBoxInfo.js
+++ src/gameAPI/fetchEachServiceBoxInfo.js
@@ -2,7 +2,7 @@ const fetchEachServiceBoxInfo = async (boxId) => {
     const apiUrl = process.env.REACT_APP_API_ENDPOINT;
     const response = await fetch(`${apiUrl}/api/games/servicePlaylist/${boxId}`);
 
-    console.log('요청을 보낸 주소는 = '+`${apiUrl}/api/games/servicePlaylist/${boxId}`)
+    // console.log('요청을 보낸 주소는 = '+`${apiUrl}/api/games/servicePlaylist/${boxId}`)
   
     if (!response.ok) {
       throw new Error('게임박스 정보 가져오기 실패');
